d822697d1e73f3c3f9ecd525879df2850ebd6795
hoangtrang0610 <69054102+hoangtrang0610@users.noreply.github.com>
2021-01-13 11:12:20 +0700

    add update, delete
    add update, delete

--- MISA.CukCuk/MISA.CukCuk.Api/Controllers/CustomersController.cs
+++ MISA.CukCuk/MISA.CukCuk.Api/Controllers/CustomersController.cs
@@ -24,10 +24,10 @@ namespace MISA.CukCuk.Api.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            var connectionString = "";
+            var connectionString = "User Id=nvmanh;Host=103.124.92.43;Port=3306;Password=12345678;Database=MISACukCuk_MF657_HTTRANG;";
             IDbConnection dbConnection = new MySqlConnection(connectionString);
-
-            return Ok(new string[] { "value1", "value2" });
+            var customers = dbConnection.Query<Customer>("Proc_GetCustomers", commandType: CommandType.Text);
+            return Ok(customers);
         }
 
         /// <summary>
@@ -43,25 +43,119 @@ namespace MISA.CukCuk.Api.Controllers
             return Ok("value");
         }
 
-        // POST api/<ValuesController>
+        /// <summary>
+        /// thêm 1 khách hàng
+        /// </summary>
+        /// <returns>Danh sách khách hàng</returns>
+        /// CreatedBy: HTTrang(09/01/2021)
         [HttpPost]
         public IActionResult Post(Customer customer)
         {
-            return Ok(Created("Add Customer", customer));
+            //validate dữ liệu
+            //check trùng mã
+            var customerCode = customer.CustomerCode;
+            if (string.IsNullOrEmpty(customerCode))
+            {
+                var msg = new
+                {
+                    devMsg = new { fieldName = "CustomerCode", msg = "Mã khách hàng không được  phép để trống" },
+                    usermsg = "Mã khách hàng không được  phép để trống",
+                    Code = 999,
+                };
+                return BadRequest(msg);
+            }
+            //Check trùng mã
+            var connectionString = "User Id=nvmanh;Host=103.124.92.43;Port=3306;Password=12345678;Database=MISACukCuk_MF657_HTTRANG;";
+            IDbConnection dbConnection = new MySqlConnection(connectionString);
+            var res = dbConnection.Query<Customer>("Proc_GetCustomerByCode", new { CustomerCode = customerCode }, commandType: CommandType.StoredProcedure);
+            if (res.Count() > 0)
+            {
+                return BadRequest("Mã khách đã tồn tại");
+            }
+            var properties = customer.GetType().GetProperties();
+            var parameters = new DynamicParameters();
+            foreach (var property in properties)
+            {
+                var propertyName = property.Name;
+                var propertyValue = property.GetValue(customer);
+                var propertyType = property.PropertyType;
+                if (propertyType == typeof(Guid) || propertyType == typeof(Guid?))
+                {
+                    parameters.Add($"@{propertyName}", propertyValue, DbType.String);
+                }
+                else
+                {
+                    parameters.Add($"@{propertyName}", propertyValue);
+                }
+            }
+            var rowEffects = dbConnection.Execute("Proc_InsertCustomer", parameters, commandType: CommandType.StoredProcedure);
+            if (rowEffects > 0)
+            {
+                return Created("abc", customer);
+            }
+            else
+            {
+                return NoContent();
+            }
         }
 
-        // PUT api/<ValuesController>/5
-        [HttpPut("{id}")]
-        public IActionResult Put(int id, [FromBody] string value)
+        /// <summary>
+        /// cập nhật thông tin 1 khách hàng
+        /// </summary>
+        /// <returns>Danh sách khách hàng</returns>
+        /// CreatedBy: HTTrang(13/01/2021)
+        [HttpPut]
+        public IActionResult Put([FromBody] Customer value)
         {
-            return Ok(1);
+            var connectionString = "User Id=nvmanh;Host=103.124.92.43;Port=3306;Password=12345678;Database=MISACukCuk_MF657_HTTRANG;";
+            IDbConnection dbConnection = new MySqlConnection(connectionString);
+            var properties = value.GetType().GetProperties();
+            var parameters = new DynamicParameters();
+            foreach (var property in properties)
+            {
+                var propertyName = property.Name;
+                var propertyValue = property.GetValue(value);
+                var propertyType = property.PropertyType;
+                if (propertyType == typeof(Guid) || propertyType == typeof(Guid?))
+                {
+                    parameters.Add($"@{propertyName}", propertyValue, DbType.String);
+                }
+                else
+                {
+                    parameters.Add($"@{propertyName}", propertyValue);
+                }
+            }
+            var res = dbConnection.QueryFirstOrDefault<Customer>("Proc_GetCustomerById", new { CustomerId = value.CustomerId.ToString() }, commandType: CommandType.StoredProcedure);
+            if (res != null)
+            {
+                var rowEffects = dbConnection.Execute("Proc_UpdateCustomer", parameters, commandType: CommandType.StoredProcedure);
+                return Ok("Cập nhật thành công");
+            }
+            else
+            {
+                return Ok("Không có khách hàng có mã trên");
+            }
         }
 
-        // DELETE api/<ValuesController>/5
+        /// <summary>
+        /// xóa 1 khách hàng
+        /// </summary>
+        /// <returns>Danh sách khách hàng</returns>
+        /// CreatedBy: HTTrang(13/01/2021)
         [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
+        public IActionResult Delete(Guid id)
         {
-            return Ok("delete");
+            var connectionString = "User Id=nvmanh;Host=103.124.92.43;Port=3306;Password=12345678;Database=MISACukCuk_MF657_HTTRANG;";
+            IDbConnection dbConnection = new MySqlConnection(connectionString);
+            var rowEffects = dbConnection.Execute("DELETE FROM  Customer WHERE CustomerId ="+"\""+id.ToString()+"\"", commandType: CommandType.Text);
+            if (rowEffects>0)
+            {
+                return Ok("Xóa thành công");
+            }
+            else
+            {
+                return Ok("Không có khách hàng có mã trên");
+            }
         }
     }
 }
